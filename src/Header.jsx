4a3948b664e07f9fe7bd039b43d78ea28db7c447
priegelsdev <priegelsdev@gmail.com>
2023-01-17 13:54:13 +0100

    add styling for header

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
 export default function Header() {
   return (
     <header>
-      <img src={profilePic} />
+      <img className="profile-pic" src={profilePic} />
       <div className="card-title">
         <h2>Laura Smith</h2>
         <h5>Frontend Developer</h5>
@@ -14,8 +14,8 @@ export default function Header() {
       </div>
 
       <div className="card-contact">
-        <button className="mail-btn"><FontAwesomeIcon icon={faEnvelope} />Email</button>
-        <button className="linkedin-btn"><img src="./src/assets/linkedin.png" />LinkedIn</button>
+        <button className="mail-btn"><FontAwesomeIcon className="button-icon" icon={faEnvelope} />Email</button>
+        <button className="linkedin-btn"><img className="button-icon" src="./src/assets/linkedin.png" />LinkedIn</button>
       </div>
     </header>
   )
